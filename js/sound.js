53370a883b56fa4382394ab28f308e242169e084
rhettdevlin <rhettd13@gmail.com>
2019-08-01 13:59:33 -0700

    Added apk download, new pictures, new titles, alarms for everyone

--- js/sound.js
+++ js/sound.js
@@ -1,14 +1,18 @@
 var richardMusic = new Audio('sounds/nogod.mp3');
 var scottMusic = new Audio('sounds/frontierPsyCropped.mp3')
+var andrewMusic = new Audio('sounds/volatile.mp3')
+var rhettMusic = new Audio('sounds/choppa.mp3')
+var austinMusic = new  Audio('sounds/bolt.mp3')
 
-function playSound(music){
-    if (!music.paused) {
+
+function playSound(music, id){
+    if (!music.paused ) {
         music.pause()
-        document.getElementById("richardSound").innerHTML = "Play my Alarm"
-
-    } else {
+        document.getElementById(id).innerHTML = "Play my Alarm"
+    }
+     else {
         music.play()
-        document.getElementById("richardSound").innerHTML = "Pause Alarm"
+        document.getElementById(id).innerHTML = "Pause Alarm"
 
     }
 }
